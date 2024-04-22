fd2d853af957283180a312dc019acb8aee83a216
wandersonwendel <wandsolopes@alu.ufc.br>
2024-04-22 15:59:01 -0300

    feat: Implementa login e logout no app usuarios - Implementa o template e a view de login, verificando os campus username e senha de um usuário que já esteja no banco de dados, - Implementa a view de logout, redirecioando o usuário a tela de login, novamente, caso acesse a url 'sair' manualmente (teste), - Modifica 'cadastro.html' para adicionar a url de 'login'.

--- usuarios/views.py
+++ usuarios/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, redirect
 # contrib, pasta com apps já instalados (auth), de autenticação,
 # e de models, onde ficam as tabelas do bd, importe a tabela User.
 from django.contrib.auth.models import User
+from django.contrib import auth
 from django.contrib.messages import constants
 from django.contrib import messages
 
@@ -46,3 +47,23 @@ def cadastro(request):
         except:
             print("Erro 4")
             return redirect('/usuarios/cadastro')
+        
+def login(request):
+    if request.method == "GET":
+        return render(request, 'login.html')
+    elif request.method == "POST":
+        username = request.POST.get('username')
+        senha = request.POST.get("senha")
+        user = auth.authenticate(request, username=username, password=senha)
+        
+        if user:
+            auth.login(request, user)
+            return redirect('/pacientes/home')
+        
+        messages.add_message(request, constants.ERROR, 'Usuário ou senha incorretos')
+        return redirect('/usuarios/login')
+    
+def sair(request):
+    auth.logout(request)
+    return redirect('/usuarios/login')
+
